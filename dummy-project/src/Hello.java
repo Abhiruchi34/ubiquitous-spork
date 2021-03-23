a7a70fcf79c5b73fc7d24dc4748b5747b2b0ce6a
Abhiruchi <shendkarabhiruchi@gmail.com>
2021-03-23 17:48:45 +0530

    I also made changes

--- dummy-project/src/Hello.java
+++ dummy-project/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 	public void method1() {
 		System.out.println("method1");
-		System.out.println("hello from method 1");
+		System.out.println("hello from method one");
 		System.out.println("3rd line added");
 		System.out.println("4th line was added");
 		
